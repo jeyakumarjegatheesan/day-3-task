a7b1a8e0c1bf7279a794a4fc2fa061cdcf39679d
jeyakumarjegatheesan <115686336+jeyakumarjegatheesan@users.noreply.github.com>
2022-11-09 12:03:34 +0530

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,46 @@
+const resume=[{
+"Position" : {
+    "Jeyakumar" : "Project Engineer"
+},
+
+"Contact deatils" : {
+    "Mobile number" : "8925777275",
+    "Email Id" : " jjeyakumar1989@gmail.com"
+},
+
+"Carreer snapshot": {
+    "Project Engineer" : "Abuhafidth & partners trade.co.llc ",
+    "Quality Engineer" : "Rams Engineering Pvt lmtd",
+    "Production associate" : "Sl lumax pvt lmtd"
+},
+
+"Academic attainment":{
+    "Qualification" : "BE Mechanial engineer",
+    "year of passing" : "2012",
+    "Total years of experience" : "8years"
+},
+"It Proficiency" : "MS office, Auto cad",
+"Personal details" :
+{
+    "Name" : "Jeyakumar",
+    "Age" : "32",
+    "Date of birth" : "18 09 1989",
+    "Gender" : "Male"
+}}] 
+// for(var i=0; i<resume.length; i++){
+//     console.log(resume[i]);            => forloop
+// }
+
+// for (var i in resume) {
+//   console.log(resume[i]);            => for in loop
+// }
+
+// for(var obj of resume){
+//     console.log(obj);               => for of loop
+// }
+
+    //  resume.forEach(function(data, index) {        for each loop
+    // console.log(data);
+    //  })
+
+
